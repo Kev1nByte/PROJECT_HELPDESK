9cd1b0af59ebe74428ff65d07a07d50a814304d5
Kev1nByte <kevinmonterozapata@gmail.com>
2023-12-20 23:59:26 -0500

    Creation Login

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+    require_once("config/conexion.php");
+    if(isset($_POST["enviar"]) and $_POST["enviar"]=="si"){
+        require_once("models/Usuario.php");
+        $usuario = new Usuario();
+        $usuario->login();
+    }    
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head lang="es">
@@ -23,31 +34,25 @@
     <div class="page-center">
         <div class="page-center-in">
             <div class="container-fluid">
-                <form class="sign-box">
+                <form class="sign-box" action="" method="post" id="login_form">
                     <div class="sign-avatar">
                         <img src="public/img/avatar-sign.png" alt="">
                     </div>
-                    <header class="sign-title">Acceso</header>
+                    <header class="sign-title">Acceso</header>              
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="E-Mail or Phone"/>
+                        <input type="text" id="usu_correo" name="usu_correo" Class="form-control" placeholder="E-Mail"/>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password"/>
+                        <input type="password" id="usu_pass" name="usu_pass" class="form-control" placeholder="Password"/>
                     </div>
                     <div class="form-group">
-                        <div class="checkbox float-left">
-                            <input type="checkbox" id="signed-in"/>
-                            <label for="signed-in">Keep me signed in</label>
-                        </div>
                         <div class="float-right reset">
-                            <a href="reset-password.html">Reset Password</a>
+                            <a href="reset-password.html">Cambiar Contraseña</a>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-rounded">Sign in</button>
-                    <p class="sign-note">New to our website? <a href="sign-up.html">Sign up</a></p>
-                    <!--<button type="button" class="close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>-->
+                    <input type="hidden" name="enviar" class="form-control" value="si"></input>
+                    <button type="submit" class="btn btn-rounded">Acceder</button>
+                    
                 </form>
             </div>
         </div>
