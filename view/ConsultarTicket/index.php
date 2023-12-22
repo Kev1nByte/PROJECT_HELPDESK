79b5b578a990c3265cbf5015f5c15fc0da681629
Kev1nByte <kevinmonterozapata@gmail.com>
2023-12-21 23:08:07 -0500

    Layout Template Pt.1

---
+++ view/ConsultarTicket/index.php
@@ -0,0 +1,1 @@
+<h1>Consutlar Ticket</h1>
--- view/Home/index.php
+++ view/Home/index.php
@@ -1,1 +1,34 @@
-<h1>home</h1>
+<!DOCTYPE html>
+<html>
+<head lang="es">
+
+    <?php
+        require_once("../MainHead/head.php");
+    ?>
+	
+    <title>Kevin Byte</title>
+</head>
+<body class="with-side-menu">
+
+    <?php
+        require_once("../MainHeader/header.php");
+    ?>
+
+	<div class="mobile-menu-left-overlay"></div>
+	
+    <?php
+        require_once("../MainNav/nav.php");
+    ?>
+
+	<div class="page-content">
+		<div class="container-fluid">
+			Blank page.
+		</div><!--.container-fluid-->
+	</div><!--.page-content-->
+
+    <?php
+        require_once("../MainJs/js.php");
+    ?>
+
+</body>
+</html>
