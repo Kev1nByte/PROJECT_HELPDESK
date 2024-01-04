dba431fc1b3885e3b8258bf5bad28cb9bb5a5b3a
Kev1nByte <kevinmonterozapata@gmail.com>
2024-01-03 19:28:54 -0500

    Ticket Detail Creation Pt.3

---
+++ docs/helpdesk (ticketdetalle).sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-01-2024 a las 22:58:38
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `helpdesk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `td_ticketdetalle`
+--
+
+CREATE TABLE `td_ticketdetalle` (
+  `tickd_id` int(11) NOT NULL,
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `tickd_descrip` mediumtext NOT NULL,
+  `fech_crea` datetime NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `td_ticketdetalle`
+--
+
+INSERT INTO `td_ticketdetalle` (`tickd_id`, `tick_id`, `usu_id`, `tickd_descrip`, `fech_crea`, `est`) VALUES
+(1, 1, 2, 'Te respondo', '2024-01-02 21:11:39', 1),
+(2, 1, 1, 'Soy el usuario respondiendo ', '2024-01-02 21:12:14', 1),
+(3, 1, 2, 'Para resolver un problema reinicia tu equipo', '2024-01-02 21:12:40', 1),
+(4, 1, 1, 'Si con eso se resolvió el problema ', '2024-01-02 21:13:25', 1),
+(5, 1, 2, 'Muchas gracias por su confirmación, por favor cerrar el ticket', '2024-01-02 21:14:04', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_categoria`
+--
+
+CREATE TABLE `tm_categoria` (
+  `cat_id` int(11) NOT NULL,
+  `cat_nom` varchar(150) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tm_categoria`
+--
+
+INSERT INTO `tm_categoria` (`cat_id`, `cat_nom`, `est`) VALUES
+(1, 'Hardware', 1),
+(2, 'Software', 1),
+(3, 'Incidencia', 1),
+(4, 'Petición de Servicio', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_ticket`
+--
+
+CREATE TABLE `tm_ticket` (
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `tick_titulo` varchar(240) NOT NULL,
+  `tick_descrip` mediumtext NOT NULL,
+  `tick_estado` varchar(15) DEFAULT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tm_ticket`
+--
+
+INSERT INTO `tm_ticket` (`tick_id`, `usu_id`, `cat_id`, `tick_titulo`, `tick_descrip`, `tick_estado`, `fech_crea`, `est`) VALUES
+(1, 1, 1, 'TEST', 'TEST', 'Abierto', '2023-12-30 13:01:51', 1),
+(2, 1, 1, 'dsfdsf', 'ddf', 'Abierto', '2023-12-30 13:02:15', 1),
+(3, 1, 1, 'test', '3432', 'Abierto', '2023-12-29 13:02:19', 1),
+(4, 1, 3, 'test', 'dfds', 'Abierto', '2023-12-28 13:02:23', 1),
+(5, 1, 1, 'test', '343', 'Abierto', '2023-12-27 13:02:26', 1),
+(6, 1, 1, 'test', '<p>34534</p>', 'Abierto', '2023-12-26 13:02:30', 1),
+(7, 1, 1, 'test', '<p><strong style=\"font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">Lorem Ipsum</strong><span style=\"font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span><br></p>', 'Abierto', '2023-12-01 13:02:33', 1),
+(8, 1, 1, 'test', '<p>ewrwe</p>', 'Abierto', '2023-12-01 13:02:36', 1),
+(9, 1, 1, 'test', '<p>sfds</p>', 'Abierto', '2023-12-30 20:24:37', 1),
+(10, 1, 4, 'test', '<p>Lorem test</p>', 'Abierto', '2023-12-30 20:25:10', 1),
+(11, 1, 1, 'test', '<p>fdfdfd</p>', 'Abierto', '2024-01-01 12:23:07', 1),
+(12, 1, 3, 'test', '<p>Prueba</p>', 'Abierto', '2024-01-01 12:23:20', 1),
+(13, 1, 4, 'PC NO ENCIENDE', '<p>fdfddfdf</p>', 'Abierto', '2024-01-01 12:23:30', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_usuario`
+--
+
+CREATE TABLE `tm_usuario` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nom` varchar(150) DEFAULT NULL,
+  `usu_ape` varchar(150) DEFAULT NULL,
+  `usu_correo` varchar(150) NOT NULL,
+  `usu_pass` varchar(29) NOT NULL,
+  `rol_id` int(11) DEFAULT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `fech_modi` datetime DEFAULT NULL,
+  `fech_elim` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla Mantenedor de Usuarios';
+
+--
+-- Volcado de datos para la tabla `tm_usuario`
+--
+
+INSERT INTO `tm_usuario` (`usu_id`, `usu_nom`, `usu_ape`, `usu_correo`, `usu_pass`, `rol_id`, `fech_crea`, `fech_modi`, `fech_elim`, `est`) VALUES
+(1, 'Kevin', 'Montero', 'kevin@gmail.com', '12345', 1, '2023-12-26 12:14:00', NULL, NULL, 1),
+(2, 'Byte', 'Montero', 'byte@gmail.com', '12345', 2, '2023-12-31 12:14:00', NULL, NULL, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `td_ticketdetalle`
+--
+ALTER TABLE `td_ticketdetalle`
+  ADD PRIMARY KEY (`tickd_id`);
+
+--
+-- Indices de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indices de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  ADD PRIMARY KEY (`tick_id`);
+
+--
+-- Indices de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `td_ticketdetalle`
+--
+ALTER TABLE `td_ticketdetalle`
+  MODIFY `tickd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  MODIFY `tick_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
