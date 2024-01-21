cc052aae1dcd1b73f45bd9d0350793d938a313f0
Kev1nByte <kevinmonterozapata@gmail.com>
2024-01-21 16:27:54 -0500

    Attach Multiple Documents to the Ticket Pt.1

--- view/NuevoTicket/index.php
+++ view/NuevoTicket/index.php
@@ -54,6 +54,12 @@
 
 						<input type="hidden" id="usu_id" name="usu_id" value="<?php echo $_SESSION["usu_id"] ?>">
 
+						<div class="col-lg-12">
+							<fieldset class="form-group">
+								<label class="form-label semibold" for="tick_titulo">Titulo</label>
+								<input type="text" class="form-control" id="tick_titulo" name="tick_titulo" placeholder="Ingrese Titulo">
+							</fieldset>
+						</div>
 						<div class="col-lg-6">
 							<fieldset class="form-group">
 								<label class="form-label semibold" for="exampleInput">Categoria</label>
@@ -64,8 +70,8 @@
 						</div>
 						<div class="col-lg-6">
 							<fieldset class="form-group">
-								<label class="form-label semibold" for="tick_titulo">Titulo</label>
-								<input type="text" class="form-control" id="tick_titulo" name="tick_titulo" placeholder="Ingrese Titulo">
+								<label class="form-label semibold" for="exampleInput">Documentos Adicionales</label>
+								<input type="file" name="fileElem" id="fileElem" class="form-control" multiple>
 							</fieldset>
 						</div>
 						<div class="col-lg-12">
