98eb3b329d1afe640ecf0e1d62aaae328dfe3045
Kev1nByte <kevinmonterozapata@gmail.com>
2024-01-22 15:32:09 -0500

    Attach Multiple Documents to the Ticket Pt.3

--- controller/ticket.php
+++ controller/ticket.php
@@ -17,6 +17,27 @@
             if (is_array($datos)==true and count($datos)>0){
                 foreach ($datos as $row){
                     $output["tick_id"] = $row["tick_id"];
+
+                    if ($_FILES['files']['name']==0){
+
+                    }else{
+                        $countfiles = count($_FILES['files']['name']);
+                        $ruta = "../public/document/".$output["tick_id"]."/";
+                        $files_arr = array();
+
+                        if (!file_exists($ruta)) {
+                            mkdir($ruta, 0777, true);
+                        }
+
+                        for ($index = 0; $index < $countfiles; $index++) {
+                            $doc1 = $_FILES['files']['tmp_name'][$index];
+                            $destino = $ruta.$_FILES['files']['name'][$index];
+
+                            $documento->insert_documento( $output["tick_id"],$_FILES['files']['name'][$index]);
+
+                            move_uploaded_file($doc1,$destino);
+                        }
+                    }
                 }
             }
             echo json_encode($datos);
