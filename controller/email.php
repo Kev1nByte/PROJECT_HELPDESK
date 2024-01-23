bf4c613d1f3bf0e5ee6e5ef9cd0ce798ed1a707b
Kev1nByte <kevinmonterozapata@gmail.com>
2024-01-23 12:28:33 -0500

    Email Ticket Alerts Pt.4 - Finished

--- controller/email.php
+++ controller/email.php
@@ -10,5 +10,12 @@
         case "ticket_abierto":
             $email->ticket_abierto($_POST["tick_id"]);
             break;
+        case "ticket_cerrado":
+            $email->ticket_cerrado($_POST["tick_id"]);
+            break;
+
+        case "ticket_asignado":
+            $email->ticket_asignado($_POST["tick_id"]);
+            break;
     }
 ?>
